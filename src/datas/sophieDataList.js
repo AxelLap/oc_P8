d3d9cda9095017cd019533270d289c261696c612
Axel LAPLACE <axellaplace1709@gmail.com>
2023-12-20 10:01:01 +0100

    useless files deleted

--- src/datas/sophieDataList.js
+++
@@ -1,8 +0,0 @@
-export const sophieDataList = {
-
-    'image1': '/images/sophieSlides/sophie-s-1.png',
-    'image2': '/images/sophieSlides/sophie-s-2.img',
-    'image3': '/images/sophieSlides/sophie-s-3.img'
-
-
-}
